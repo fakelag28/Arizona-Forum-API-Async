5facb98bb7f4f7e83547c9e44a7c04f6ce08dae1
Mikhail <90392475+fakelag28@users.noreply.github.com>
2025-06-16 14:43:38 +0300

    .

--- examples/category_threads.py
+++ examples/category_threads.py
@@ -2,9 +2,9 @@ import asyncio
 import arizona_forum_async as arz_api
 
 async def main():
-    cookies = {"xf_user": "1058071%2Cpyl5mfwp27ag59SwiBEmyB58uZ2OFqW3KFGoFaf4",
-              "xf_tfa_trust": "fifcs_70CGMxlGN6y6dofRwSR6M7LtLS", 
-              "xf_session": "OmIBQ19cobJRq47LHiT4U4gUmtGWseyp"}
+    cookies = {"xf_user": "123",
+              "xf_tfa_trust": "123", 
+              "xf_session": "123"}
 
     api = arz_api.ArizonaAPI(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 YaBrowser/24.12.0.0 Safari/537.36", cookie=cookies)
 
